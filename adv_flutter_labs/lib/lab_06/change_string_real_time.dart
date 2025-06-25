b489f8d552b339bbd4e3b3b65181852ddb64439a
neelgohelh <neelgohel8887@gmail.com>
2025-06-25 19:08:59 +0530

    25-06-2025

--- adv_flutter_labs/lib/lab_06/change_string_real_time.dart
+++ adv_flutter_labs/lib/lab_06/change_string_real_time.dart
@@ -1,43 +1,48 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../utils/import_export.dart';
+class Controller extends GetxController {
+  var name = "".obs;
+  TextEditingController nameController = TextEditingController();
+}
 
 class ChangeStringRealTime extends StatelessWidget {
   ChangeStringRealTime({super.key});
-  RxString name = "".obs;
-  TextEditingController nameController = TextEditingController();
+
+  final Controller controller = Get.put(Controller());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Changes using getx with stless widget")),
-      body: Column(
-        children: [
-          TextFormField(
-            controller: nameController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
+      appBar: AppBar(title: Text("Changes using GetX with StatelessWidget")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller.nameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
               ),
+              onChanged: (value) {
+                controller.name.value = value;
+              },
             ),
-          ),
-          SizedBox(height: 10),
+            SizedBox(height: 10),
+            Obx(() => Text("${controller.name.value}")),
 
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                controller.nameController.clear();
+                controller.name.value = "";
+              },
+              child: Text("Clear"),
             ),
-          ),
-
-          SizedBox(height: 10),
-          TextButton(
-            onPressed: () {
-              nameController.text = "";
-            },
-            child: Text("Clear"),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
