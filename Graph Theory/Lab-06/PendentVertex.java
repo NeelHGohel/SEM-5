b96ac8e4d735d4127b3f6ca800108364b3517d56
neelgohelh <neelgohel8887@gmail.com>
2025-07-10 09:20:10 +0530

    10-07-2025

--- Graph Theory/Lab-06/PendentVertex.java
+++ Graph Theory/Lab-06/PendentVertex.java
@@ -1,23 +1,23 @@
 
-public class MaxDepth {
+public class PendentVertex {
     public static void main(String[] args) {
         Node root = new Node(3);
         root.left = new Node(9);
         root.right = new Node(20);
         root.right.left = new Node(15);
         root.right.right = new Node(7);
-
-        System.out.println(maxDepth(root));
-
+        System.out.println(pendent(root));
     }
 
-    public static int maxDepth(Node root) {
+    static int pendent(Node root) {
+
         if (root == null) {
             return 0;
         }
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return 1 + Integer.max(left, right);
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return pendent(root.left) + pendent(root.right);
     }
 }
 
