5c4c359bb9f4e41cb38f65858c4f1d7dd40b72a5
neelgohelh <neelgohel8887@gmail.com>
2025-08-19 07:52:17 +0530

    19-08-2025

--- Computer Network/Lab 06/TCP/Client.java
+++ Computer Network/Lab 06/TCP/Client.java
@@ -1,20 +1,12 @@
 package TCP;
-
-// Demonstrating Client-side Programming
 import java.io.*;
 import java.net.*;
-
 public class Client {
-  
-    // Initialize socket and input/output streams
     private Socket s = null;
     private DataInputStream in = null;
     private DataOutputStream out = null;
 
-    // Constructor to put IP address and port
-    public Client(String addr, int port)
-    {
-        // Establish a connection
+    public Client(String addr, int port){
         try {
             s = new Socket(addr, port);
             System.out.println("Connected");
@@ -33,11 +25,7 @@ public class Client {
             System.out.println(i);
             return;
         }
-
-        // String to read message from input
         String m = "";
-
-        // Keep reading until "Over" is input
         while (!m.equals("Over")) {
             try {
                 m = in.readLine();
@@ -47,8 +35,6 @@ public class Client {
                 System.out.println(i);
             }
         }
-
-        // Close the connection
         try {
             in.close();
             out.close();
@@ -58,7 +44,6 @@ public class Client {
             System.out.println(i);
         }
     }
-
     public static void main(String[] args) {
         Client c = new Client("127.0.0.1", 5000);
     }
